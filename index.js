5a4aed7380884bcfd94f6fdef4a20b689f8e4172
prashant Karki <prashantkarki0617@gmail.com>
2026-01-03 15:36:11 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const jwt = require('jsonwebtoken');
+
+app.post("/userSignup",(req,res)=>{
+
+});
+
+app.post("/userLogin",(req,res)=>{
+
+});
+
+app.post("/purchaseCourse",(req,res)=>{
+
+});
+
+app.post("/seeAllCourses",(req,res)=>{
+
+});
+
+app.post("/seePurchasedCourse",(req,res)=>{
+
+});
+
+app.post("/adminSignup",(req,res)=>{
+
+});
+
+app.post("/adminLogin",(req,res)=>{
+
+});
+
+app.post("/createCourse",(req,res)=>{
+
+});
+
+app.post("/deleteCourse",(req,res)=>{
+
+});
+
+app.post("/addContentToCourse",(req,res)=>{
+
+});
