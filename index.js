ac4ebf08c727c3366a20da99ec06d85686e7a769
devprashant17 <prashantkarki0617@gmail.com>
2026-01-06 09:48:17 +0545

    .env file added after changingf the db url and all sensitive data

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+require('dotenv').config();
+const dbUrl = process.env.DATABASE_URL;
+const PORT = process.env.PORT;
+const JWT_SECRET = process.env.JWT_SECRET;
+
 const express = require('express');
 const app = express();
 const jwt = require('jsonwebtoken');
+
+
+
 
 const mongoose = require('mongoose');
 
@@ -13,8 +21,8 @@ app.use('/course',courseRouter);
 app.use('/admin',adminRouter);
 
 async function listen(){
-    await mongoose.connect('mongodb+srv://prashantkarki:nHTQ99lLTXjSI6aI@cluster0.bc0zy5d.mongodb.net/COURSE-SELLING-APP');
-    app.listen(3000);
+    await mongoose.connect(dbUrl);
+    app.listen(PORT);
 }
 
 listen();
